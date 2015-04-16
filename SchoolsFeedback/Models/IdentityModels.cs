b7572be748dee77fc1d52118c5a2e0e074fbd20c
Vitya <kalinin.vitya@gmail.com>
2015-04-16 19:05:04 +0300

    Message Model added

--- SchoolsFeedback/Models/IdentityModels.cs
+++ SchoolsFeedback/Models/IdentityModels.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
 
 namespace SchoolsFeedback.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public virtual ICollection<Message> Messages  { get; set; }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
@@ -13,5 +17,6 @@ namespace SchoolsFeedback.Models
             : base("DefaultConnection")
         {
         }
+        public DbSet<Message> Messages { get; set; }
     }
 }
---
+++ SchoolsFeedback/Models/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SchoolsFeedback.Models
+{
+    public class Message
+    {
+        public int ID { get; set; }
+        public string Theme { get; set; }
+        public string Text { get; set; }
+        public string Status{ get; set; }
+        public DateTime DateCreated { get; set; }
+        public string UserID { get; set; }
+        [ForeignKey("UserID")]
+        public virtual ApplicationUser User { get; set; }
+    }
+
+}
