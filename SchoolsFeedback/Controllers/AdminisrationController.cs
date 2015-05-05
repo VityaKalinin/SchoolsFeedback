8fc456baef396477210c89e18f2a8f87cdf2b272
Vitya <kalinin.vitya@gmail.com>
2015-05-05 19:07:33 +0300

    Layout modified

---
+++ SchoolsFeedback/Controllers/AdminisrationController.cs
@@ -0,0 +1,23 @@
+﻿using SchoolsFeedback.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolsFeedback.Controllers
+{
+    public class AdminisrationController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        //
+        // GET: /Adminisration/
+        public ActionResult AllMessages()
+        {
+            var messages = db.Messages.ToList();
+            return View(messages);
+        }
+
+
+	}
+}
